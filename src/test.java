2f175ac313979b3b9a782229fb2efab9868b5fb6
LobeliaComputing <81894304+LobeliaComputing@users.noreply.github.com>
2021-04-08 01:58:42 +0900

    Update test.java

--- src/test.java
+++ src/test.java
@@ -4,18 +4,19 @@ import java.util.function.Function;
 
 public class test{
 
-    static String print(String s){
+    static <T> T print(T s){
         System.out.println(s);
         return s;
     }
+
     public static void main(String[] args){
       print("Start");
 
-      Map<String, Function<String, Object>> map = new HashMap<String, Function<String, Object>>(){{
+      Map<String, Function<Object, Object>> map = new HashMap<String, Function<Object, Object>>(){{
           put("print", test::print);
       }};
-      
-      map.get("print").apply("aaa");
+      map.get("print").apply("print!");
+      map.get("print").apply(1234567890);
       print("End");
     }
  }
